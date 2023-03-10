b388789d2b7973dfd9a7da10bbee32c2f10102d5
Watheq Attrah <novaatrah@gmail.com>
2023-03-10 14:36:59 -0700

    Complete Looping Lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+
+
+function writeCards(names, cards){
+  const messNames = [];    
+  for (let i = 0; i < names.length; i++){
+          let message = (`Thank you, ${names[i]}, for the wonderful ${cards} gift!`);
+    
+      messNames.push(message);           
+      }
+return messNames;
+}
+writeCards(["Charlie", "Samip", "Ali"], "birthday");
+
+
+function countDown(number){
+
+while (number >= 0){
+console.log(number--);}
+return
+}
+countDown (10);
